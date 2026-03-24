0dd3f0f960771285f73ce96c81d883dbbb8efd10
Supra-Samiksha <supradkl06@gmail.com>
2026-03-24 22:46:43 +0530

    Update KafkaListeners

--- src/test/java/com/jpmc/midascore/listeners/KafkaListeners.java
+++ src/test/java/com/jpmc/midascore/listeners/KafkaListeners.java
@@ -1,7 +1,9 @@
 package com.jpmc.midascore.listeners;
 
 import com.jpmc.midascore.component.DatabaseConduit;
+import com.jpmc.midascore.component.IncentiveServiceClient;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.foundation.TransactionRecord;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,25 +16,36 @@ public class KafkaListeners {
     @Autowired
     private DatabaseConduit databaseConduit;
 
+    @Autowired
+    private IncentiveServiceClient incentiveServiceClient;
+
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
     public void listen(Transaction transaction) {
-        // 1. Fetch the actual User objects from the DB using the IDs in the Kafka message
+        // 1. Fetch users from database
         UserRecord sender = databaseConduit.findById(transaction.getSenderId());
         UserRecord recipient = databaseConduit.findById(transaction.getRecipientId());
 
-        // 2. The Validation Rules
+        // 2. Validation: Ensure both exist and sender has enough funds
         if (sender != null && recipient != null && sender.getBalance() >= transaction.getAmount()) {
             
-            // 3. Update the balances
-            sender.setBalance(sender.getBalance() - transaction.getAmount());
-            recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+            // 3. External API Call: Get the incentive amount
+            Incentive incentive = incentiveServiceClient.getIncentive(transaction);
+            float rewardAmount = (incentive != null) ? incentive.getAmount() : 0f;
 
-            // 4. Save the updated users back to the database
+            // 4. Update Balances
+            // Sender pays only the transaction amount
+            sender.setBalance(sender.getBalance() - transaction.getAmount());
+            
+            // Recipient gets the transaction amount + the external incentive
+            recipient.setBalance(recipient.getBalance() + transaction.getAmount() + rewardAmount);
+
+            // 5. Persist Changes
             databaseConduit.save(sender);
             databaseConduit.save(recipient);
 
-            // 5. Create and save the permanent record of this transaction
+            // 6. Record the transaction (Include the incentive in your record if your entity supports it)
             TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount());
+            // Note: If you added an incentive field to TransactionRecord in Task 4, set it here.
             databaseConduit.save(record);
         }
     }
